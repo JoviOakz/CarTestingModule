c900ad5949281ccbdfc328b0a703af1611d39060
MuryloSaladino <murylosaladino@gmail.com>
2024-04-19 08:32:33 -0300

    firebase connection

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import obd
+import firebase_admin
+import json
+import serial
+from firebase_admin import credentials
+from firebase_admin import db
+import time
 
+
+cred = credentials.Certificate("./certificate.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://cartestingmodule-default-rtdb.firebaseio.com/"
+})
+
+ser = serial.Serial("/dev/ttyUSB0", 9600)
+ref = db.reference("/collectedData")
+
+
+while True:
+    json_serial = json.loads(ser.readline())
+    ref.push(json_serial)
 
 
 connection = obd.Async(fast=False)
