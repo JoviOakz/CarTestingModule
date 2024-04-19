0b421b0b84de2bb71e6866da5486132d6c15a345
MuryloSaladino <murylosaladino@gmail.com>
2024-04-19 08:56:44 -0300

    save to firebase functions

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
-import obd
-import firebase_admin
-import json
-import serial
 from firebase_admin import credentials
 from firebase_admin import db
+import firebase_admin
 import time
+import obd
 
+connection = obd.Async(fast=False)
 
 cred = credentials.Certificate("./certificate.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': "https://cartestingmodule-default-rtdb.firebaseio.com/"
 })
 
-ser = serial.Serial("/dev/ttyUSB0", 9600)
-ref = db.reference("/collectedData")
+def firebase_push(data_name):
+    def callback(data): 
+        ref = db.reference(f"/{data_name}/{time.time()}")
+        ref.set(data)
+    return callback
 
 
-while True:
-    json_serial = json.loads(ser.readline())
-    ref.push(json_serial)
-
-
-connection = obd.Async(fast=False)
-
-connection.watch(obd.commands.RPM, )
-connection.watch(obd.commands.SPEED, )
+connection.watch(obd.commands.RPM, firebase_push("rpm"))
+connection.watch(obd.commands.SPEED, firebase_push("speed"))
 
 connection.close()
